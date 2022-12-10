952c4768cf4c36758ec6bdb53567e8077a96e6c7
Tejz0560 <tejbidwai@gmail.com>
2022-12-10 10:22:51 +0530

    add json

--- app.py
+++ app.py
@@ -10,4 +10,8 @@ def index():
 def register():
     return render_template('register.html')
 
+@app.route("/perform_registration")
+def add_data():
+    return "Successful registration"
+
 app.run(debug = True)
