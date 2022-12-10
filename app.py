ea90a25345c1d0d6ae8668df616a28ef82950c63
Tejz0560 <tejbidwai@gmail.com>
2022-12-10 10:11:35 +0530

    registraion page added

--- app.py
+++ app.py
@@ -6,4 +6,8 @@ app = Flask(__name__)
 def index():
     return render_template('login.html')
 
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
 app.run(debug = True)
